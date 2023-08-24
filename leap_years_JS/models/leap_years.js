99c43419a227d703747047e2ea87e1a8f878509c
GPSPic <picard.guillaume47@gmail.com>
2023-08-24 17:14:37 +0100

    completed kata

--- leap_years_JS/models/leap_years.js
+++ leap_years_JS/models/leap_years.js
@@ -1,7 +1,16 @@
-const leapYear = function (number) {
+const leapYear = function (num) {
     let isLeapYear = false;
 
-
+    if (typeof num !== "number") {
+        console.log("What the hell are you doing?")
+        return null;
+    } else if (num % 400 === 0) {
+        isLeapYear = true;
+    } else if (num % 100 === 0) {
+        isLeapYear = false;
+    } else if (num % 4 === 0) {
+        isLeapYear = true;
+    }
 
     return isLeapYear;
 }
--- leap_years_JS/specs/leap_years_spec.js
+++ leap_years_JS/specs/leap_years_spec.js
@@ -1,15 +1,40 @@
 const assert = require('assert');
-const leapYear = require('../models/leap_year.js');
+const leapYear = require('../models/leap_years.js');
 
-describe('LeapYear', function() {
+describe('leapYear', function() {
 
     it('should return true if it is a typical leap year', function() {
         const actual = leapYear(1996);
         assert.strictEqual(actual, true);
     })
 
-    // it('should return false for a typical common year', function() {
-    //     const actual = leapYear(1995);
-    //     assert.strictEqual(actual, false);
-    // })
+    it('should return false for a typical common year', function() {
+        const actual = leapYear(1995);
+        assert.strictEqual(actual, false);
+    })
+
+    it('should return false if it is an atypical common year', function() {
+        const actual = leapYear(1700);
+        assert.strictEqual(actual, false);
+    })
+
+    it('should return true if it is an atypical leap year', function() {
+        const actual = leapYear(2000);
+        assert.strictEqual(actual, true);
+    })
+
+    it('should return false if it is a negative common year', function() {
+        const actual = leapYear(-35);
+        assert.strictEqual(actual, false);
+    })
+
+    it('should return true if it is an negative leap year', function() {
+        const actual = leapYear(-44);
+        assert.strictEqual(actual, true);
+    })
+
+    it('should return null if num is not a number', function() {
+        const actual = leapYear("pancakes");
+        assert.strictEqual(actual, null);
+    })
 })
