3a29f696a5a1e8858798e3e2ce5a8e4ae8ff3eef
GPSPic <picard.guillaume47@gmail.com>
2023-09-04 12:06:21 +0100

    add player chooses door, removed unnecessary classes

--- monthy_hall_python3/classes/monty_hall.py
+++ monthy_hall_python3/classes/monty_hall.py
@@ -2,13 +2,21 @@ from random import randint
 
 
 class MontyHallGame:
-    def __init__(self, player, presenter, prize):
-        self.player = player
-        self.presenter = presenter
-        self.prize = prize
+    def __init__(self):
+        self.prize = 1
         self.doors = [0,0,0]
+        self.player_first_choice_value = None
+        self.player_first_choice_index = None
+        self.final_doors = [0,0]
 
+# randomly assigns the prize value to one of the elements of the doors array
     def hide_prize(self):
-        randomized = randint(1,3)
-        self.doors[randomized] = self.prize.value
+        randomized = randint(0,2)
+        self.doors[randomized] = self.prize
+
+# saves the random door value and index choice of the player
+    def choose_first_door(self):
+        randomized = randint(0,2)
+        self.player_first_choice_value = self.doors[randomized]
+        self.player_first_choice_index = randomized
     
--- monthy_hall_python3/tests/monty_hall_test.py
+++ monthy_hall_python3/tests/monty_hall_test.py
@@ -1,8 +1,6 @@
 import unittest
 
-from classes.player import Player
-from classes.presenter import Presenter
-from classes.prize import Prize
+from archived.prize import Prize
 from classes.monty_hall import MontyHallGame
 
 from functools import reduce
@@ -10,16 +8,22 @@ from operator import add
 
 class MonthyHallTest(unittest.TestCase):
     def setUp(self):
-        self.player = Player("Bob")
-        self.presenter = Presenter("Tim")
-        self.prize = Prize(100)
-        self.monty_hall_game = MontyHallGame(self.player, self.presenter, self.prize)
+        self.monty_hall_game = MontyHallGame()
 
     # test1: hide prize behind random door, verify value by checking total value of doors array elements
     def test_can_hide_prize_behind_doors(self):
         self.assertEqual(0, reduce(lambda a, b: a+b, self.monty_hall_game.doors))
         self.monty_hall_game.hide_prize()
-        self.assertEqual(100, reduce(add, self.monty_hall_game.doors))
-        self.assertEqual(100, sum(self.monty_hall_game.doors))
+        self.assertEqual(1, reduce(add, self.monty_hall_game.doors))
+        self.assertEqual(1, sum(self.monty_hall_game.doors))
 
-    # test2: 
+    # test2: player picks a door
+    def test_player_can_choose_a_door(self):
+        self.assertEqual(False, type(self.monty_hall_game.player_first_choice_value) is int)
+        self.assertEqual(None, self.monty_hall_game.player_first_choice_index)
+        self.monty_hall_game.choose_first_door()
+        result = type(self.monty_hall_game.player_first_choice_value) is int
+        self.assertEqual(True, result)
+        self.assertEqual(True, type(self.monty_hall_game.player_first_choice_index) is int)
+
+    # test 
