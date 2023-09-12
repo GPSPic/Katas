405f7fb9b7a37dcf964d2154d8cb6bf0bc1ffc15
wenjingsheng <wenjing2018uk@gmail.com>
2023-09-12 11:11:09 +0100

    task acomplished

--- recently_used_list_JS/models/recentlyUsed.js
+++ recently_used_list_JS/models/recentlyUsed.js
@@ -1,16 +1,25 @@
-const RecentlyUsed = function (num) {
-    this.num = num
-    this.arr = []
-}
+class RecentlyUsed {
+  constructor(num) {
+    this.num = num;
+    this.arr = [];
+  }
 
-RecentlyUsed.prototype.addString = function (str) {
-    if (this.arr.length < this.num && !this.arr.includes(str)) {
-        this.arr.unshift(str)
+  getArr() {
+    return this.arr;
+  }
+
+  addString(str) {
+    if (str === "" || str === null || str === undefined) {
+    } else if (this.arr.includes(str)) {
+      this.arr = this.arr.filter(s => s!== str);
+      this.arr.unshift(str);
+    } else if (this.arr.length < this.num && !this.arr.includes(str)) {
+      this.arr.unshift(str);
+    } else if (this.arr.length === this.num) {
+      this.arr.pop();
+      this.arr.unshift(str);
     }
+  }
 }
 
-RecentlyUsed.prototype.getArr = function () {
-    return this.arr
-}
-
-module.exports = RecentlyUsed
+module.exports = RecentlyUsed;
--- recently_used_list_JS/specs/recentlyUsed_specs.js
+++ recently_used_list_JS/specs/recentlyUsed_specs.js
@@ -16,16 +16,56 @@ describe('RecentlyUsed', function() {
         recentlyUsed = new RecentlyUsed(3)
     })
 
+    it('should do add anything to the array if the string is falsy', function() {
+        recentlyUsed.addString("")
+        const actual = recentlyUsed.getArr().length
+        assert.strictEqual(actual, 0)
+    })
+
     it('should do be able to add a single string', function() {
-        // recentlyUsed.addString(a)
+        recentlyUsed.addString(a)
         const actual = recentlyUsed.getArr()[0]
         assert.strictEqual(actual, "a")
     })
 
-    // it('should do be able to add 2 unique strings', function() {
-    //     recentlyUsed(a)
-    //     const actual = recentlyUsed(b)
+    it('should do be able to add 2 unique strings', function() {
+        recentlyUsed.addString(a)
+        recentlyUsed.addString(b)
+        const actual = recentlyUsed.getArr().length 
+        assert.strictEqual(actual, 2)
+    })
 
-    //     assert.strictEqual(actual, "a")
-    // })
+    it('should push the first input when over max length', function () { 
+        recentlyUsed.addString(a)
+        recentlyUsed.addString(b)
+        recentlyUsed.addString(c)
+        recentlyUsed.addString(d)
+        const actual = recentlyUsed.getArr()[0]
+        assert.strictEqual(actual, 'd')
+        const actual2 = recentlyUsed.getArr()[2]
+        assert.strictEqual(actual2, 'b')
+    })
+
+    it("should not allow duplicate in the list and push them back to last used", function () {
+        recentlyUsed.addString(a)
+        recentlyUsed.addString(b)
+        recentlyUsed.addString(c)
+        recentlyUsed.addString(b)
+        const actual1 = recentlyUsed.getArr()[0]
+        const actual2 = recentlyUsed.getArr()[2]
+        assert.strictEqual(actual1, "b")
+        assert.notStrictEqual(actual2, "b")
+    })
+
+    it("should not allow duplicate in the list and push them back to last used", function () {
+        recentlyUsed.addString(b)
+        recentlyUsed.addString(b)
+        recentlyUsed.addString(b)
+        recentlyUsed.addString(b)
+        const actual1 = recentlyUsed.getArr().length
+        const actual2 = recentlyUsed.getArr()[0]
+        assert.strictEqual(actual1, 1)
+        assert.strictEqual(actual2, "b")
+    })
+
 })
